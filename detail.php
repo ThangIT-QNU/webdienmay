cb5f5790e6ed9dd6d6b7448df346ad60cceb7dac
ThangIT-QNU <dinhthanhthang2007@gmail.com>
2022-01-04 16:37:33 +0700

    Phát triển phần hiển thị comment ràng buộc id sản phẩm

--- detail.php
+++ detail.php
@@ -1,4 +1,3 @@
-
 <?php
 require 'layout/autoload.php';
 if(!isset($_GET['id_product'])){
@@ -10,57 +9,56 @@ if(!isset($_GET['id_product'])){
     $detail = $product->getProductById($id);
     foreach ($detail as $key => $value_detail) {
         ?>
-        <style>
-        
-        .comment{
-            margin: 10px 0;
-        }
-        
-        </style>
-    <div class="detail" style="padding-top:10px">
-        <div class="row">
-            <div class="col-md-5">
-                <img style="height:340px" class="img-responsive"
-                    src="public/image/<?php echo $value_detail['image'] ?>" alt="">
-            </div>
-            <div class="col-md-7">
-                <h3><?php echo $value_detail['name'] ?></h3>
-                <s>
-                    <h4>Giá: <?php echo $value_detail['price'] ?> VNĐ</h4>
-                </s>
-                <h4 style="color:red">Giá KM: <?php echo $value_detail['sale'] ?> VNĐ</h4>
-                
-                <form action="cart.php?id_product=<?php echo $value_detail['id'] ?>" method="POST">
+<style>
+.comment {
+    margin: 10px 0;
+}
+</style>
+<div class="detail" style="padding-top:10px">
+    <div class="row">
+        <div class="col-md-5">
+            <img style="height:340px" class="img-responsive" src="public/image/<?php echo $value_detail['image'] ?>"
+                alt="">
+        </div>
+        <div class="col-md-7">
+            <h3><?php echo $value_detail['name'] ?></h3>
+            <s>
+                <h4>Giá: <?php echo $value_detail['price'] ?> VNĐ</h4>
+            </s>
+            <h4 style="color:red">Giá KM: <?php echo $value_detail['sale'] ?> VNĐ</h4>
+
+            <form action="cart.php?id_product=<?php echo $value_detail['id'] ?>" method="POST">
                 <div class="row">
-                <div class="col-md-3">
-                   <br> Số Lượng:
+                    <div class="col-md-3">
+                        <br> Số Lượng:
+                    </div>
+                    <div class="col-md-3">
+                        <br><input type="number" value="1" min="1" class="form-control" name="qty">
+                    </div> <br>
+                    <div class="col-md-4"><input class="btn btn-success" type="submit" value="Mua hàng"></div>
                 </div>
-                <div class="col-md-3">
-                    <br><input type="number" value="1" min="1" class="form-control" name="qty"> </div>   <br> 
-                    <div class="col-md-4"><input class="btn btn-success" type="submit" value="Mua hàng"></div> 
-                </div>
-                </form>
+            </form>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="san-pham">
+                <span>Mô Tả Sản Phẩm:</span>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="san-pham">
-                    <span>Mô Tả Sản Phẩm:</span>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="desc">
-                    <p><?php echo $value_detail['description'] ?></p>
-                </div>
+        <div class="col-md-12">
+            <div class="desc">
+                <p><?php echo $value_detail['description'] ?></p>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="san-pham">
-                 <center><span>Comment</span></center>   
-                </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="san-pham">
+                <center><span>Comment</span></center>
             </div>
-            <?php
+        </div>
+        <?php
             
             if (isset($_POST['comment'])) {
                 $comment1 = $_POST['comment'];
@@ -72,17 +70,17 @@ if(!isset($_GET['id_product'])){
                 $comment = new Comment();
                 $comment->addComment($id, $user, $comment1);
             } ?>
-            <div class="col-md-12">
-                <form action="" method="POST">
-                    <label>Nhập Comment</label>
-                    <textarea name="comment" placeholder="Nhập Comment ..." class="form-control" rows="5"></textarea> <br>
-                    <input type="submit" class="form-control btn btn-success">
-                </form><br>
-            </div> 
-            <?php
+        <div class="col-md-12">
+            <form action="" method="POST">
+                <label>Nhập Comment</label>
+                <textarea name="comment" placeholder="Nhập Comment ..." class="form-control" rows="5"></textarea> <br>
+                <input type="submit" class="form-control btn btn-success">
+            </form><br>
+        </div>
+        <?php
             
             $db = new Db();
-            $sql = "SELECT * FROM comment ORDER BY id DESC";
+            $sql = "SELECT * FROM comment WHERE id_products = '$id'";
             
             if(isset($_GET['page'])){
                 $page = $_GET['page'];
@@ -98,42 +96,43 @@ if(!isset($_GET['id_product'])){
             if (count($cmt) > 0) {
                 foreach ($cmt as $key => $value_comment) {
                 ?>
-            <div class="col-md-12">
-                <div class="comment">
-                    <div class="col-xs-3 col-md-1">
-                        
-                        <i style="color: #FF5622;font-size: 3em" class="fas fa-user"></i>
+        <div class="col-md-12">
+            <div class="comment">
+                <div class="col-xs-3 col-md-1">
+
+                    <i style="color: #FF5622;font-size: 3em" class="fas fa-user"></i>
+                </div>
+                <div class="col-xs-9 col-md-11">
+
+                    <div>
+                        <?php echo $value_comment['comment']?>
                     </div>
-                    <div class="col-xs-9 col-md-11">
-                        
-                        <div>
-                            <?php echo $value_comment['comment']?>
-                        </div>
-                        <div>
-                            <span style="color: #FF5622;"><?php echo  $value_comment['name'] ?></span> - <?php echo $value_comment['created_at'] ?>
-                        </div>
-                        
-           
+                    <div>
+                        <span style="color: #FF5622;"><?php echo  $value_comment['name'] ?></span> -
+                        <?php echo $value_comment['created_at'] ?>
                     </div>
+
+
                 </div>
             </div>
-                <?php
+        </div>
+        <?php
              } 
              $db->numPagination1($sql,$page,5);
-             ?>    
-                
-             <?php
+             ?>
+
+        <?php
             }else {
             ?>
-                 <div class="col-md-12">
-                     <center>Không Có Comment Cho Sản Phẩm Này!!!</center>
-                 </div>
-                 <?php
+        <div class="col-md-12">
+            <center>Không Có Comment Cho Sản Phẩm Này!!!</center>
+        </div>
+        <?php
         }
     }
              ?>
-        </div>
     </div>
+</div>
 <?php
     
 }
