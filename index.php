40e9f77d3776f6b836176b18b93a7eda498b2f85
ThangIT-QNU <dinhthanhthang2007@gmail.com>
2022-01-11 16:28:46 +0700

    Thay banner

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require 'layout/autoload.php';
 require 'layout/header.php';
 ?>
-<img style="margin-top:30px;" height="398px" width="100%" src="./public/image/1/banner.png" alt="">
+<img style="margin-top:30px;" height="398px" width="100%" src="./public/image/1/banner2.png" alt="">
 <?php 
 require 'layout/close.php';
 require 'layout/home.php';
